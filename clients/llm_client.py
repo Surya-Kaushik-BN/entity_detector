07abb09fd3c048980964072d8dca81e9af6d55cc
Surya Kaushik B N <suryakaushik23@gmail.com>
2025-03-02 17:09:49 +0530

    Added streamlit secrets

--- clients/llm_client.py
+++ clients/llm_client.py
@@ -1,12 +1,12 @@
 import os
 from google import genai
-from dotenv import load_dotenv
-load_dotenv()
+import streamlit as st
+
 
 
 class LLMClient:
     def __init__(self,):
-        api_key = os.environ.get('GEMINI_API_KEY')
+        api_key = st.secrets["llm"]["GEMINI_API_KEY"]
         self.llm_client = genai.Client(api_key=api_key)
 
     def run(self, message: str):
